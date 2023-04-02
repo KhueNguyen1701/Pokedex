eab2d125ba2e3c598547c56c5c178f1c36a72db1
Nguyễn Huỳnh Minh Khuê <nguyenhuynhminhkhue@MacBook-Pro-cua-Nguyen-2.local>
2023-04-02 15:33:52 +0700

    sua lan 1

--- pokemon.js
+++ pokemon.js
@@ -5,21 +5,22 @@ const type = document.querySelector('.type-value')
 const height = document.querySelector('.height-value')
 const weight = document.querySelector('.weight-value')
 const bio = document.querySelector('.bio-value')
-const pokemonImage = document.querySelector('.pokemon-img2')
+// const pokemonImage = document.querySelector('.pokemon-img2')
 
 searchInput.onchange = async function (event) {
     const value = event.target.value
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${value}/`)
     const data = await res.json()
+    console.log(data)
 
-    // Em chưa lấy được phần bio
-    // const value2 = event.target.value2
-    // const res2 = await fetch(`https://pokeapi.co/api/v2/ability/${value2}/`)
-    // const data2 = await res.json()
+    const value2 = event.target.value
+    const res2 = await fetch(`https://pokeapi.co/api/v2/ability/${data.id}/`)
+    const data2 = await res2.json()
 
     name.innerHTML = data.forms[0].name || DEFAULT_VALUE
     type.innerHTML = data.types[0].type.name || DEFAULT_VALUE
     height.innerHTML = ((data.height / 10) + ' m') || DEFAULT_VALUE
     weight.innerHTML = (data.weight + ' lbs') || DEFAULT_VALUE
-    pokemonImage.setAttribute('src', `http://pokeapi.co/media/sprites/pokemon/${pokemonImage}.png`)
+    bio.innerHTML = data2.effect_entries[1].short_effect || DEFAULT_VALUE
+    // pokemonImage.setAttribute('src', `http://pokeapi.co/media/sprites/pokemon/${pokemonImage}.png`)
 }
